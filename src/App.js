25c47418b9f4e30e3a617a6a004295788b6c0c41
hanim3 <hanim0308as12@naver.com>
2022-02-15 19:28:43 +0900

    learn about useEffect(deps)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import style from "./App.module.css";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(() => {
+    // 컴포넌트가 생성되는 첫 시작에만 실행됨
+    console.log("I run only once");
+  }, []);
+  useEffect(() => {
+    // keyword가 업데이트 될 때마다 실행됨
+    if (keyword !== "" && keyword.length > 5) {
+      console.log(`I run when 'keyword' changes.`);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    // counter가 업데이트 될 때마다 실행됨
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    // keyword, counter가 업데이트 될 때마다 실행됨
+    console.log("I run when counter & keyword changes.");
+  }, [counter, keyword]);
   return (
     <div>
-      <h1 className={style.title}>Welcome back!!</h1>
-      <Button text={"Continue"} />
+      <input 
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
