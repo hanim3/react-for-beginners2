60393edbfbfc06fa2b381df01be7ab87742678a6
hanim3 <hanim0308as12@naver.com>
2022-02-15 18:30:26 +0900

    init

---
+++ src/Button.js
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+import style from "./Button.module.css";
+
+function Button({ text }) {
+  return (
+    <div>
+      <button className={style.btn}>{text}</button>
+    </div>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Button;
